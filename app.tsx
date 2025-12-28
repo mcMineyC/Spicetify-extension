875f9467308548d84792ccc9dbee70d686c06647
mcMineyC <jedi.master@falconhosting.net>
2025-12-28 15:04:49 -0800

    update for new version

--- app.tsx
+++ app.tsx
@@ -1,24 +1,16 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
-
-const io = new Server({
-  // options
-});
+import { io } from "socket.io-client";
 
 function sleep(ms:number) {return new Promise(resolve => setTimeout(resolve, ms));}
 
 async function main() {
   var lastProgressUpdate = Date.now();
   await sleep(200)
-  io.on("connection", () => {
-  socket.emit("playbackState", p.data.isPaused ? "Paused" : "Playing")
-  socket.emit("progress", p.data)
-  socket.emit("metadata",Spicetify.Queue.track.contextTrack.metadata)
-  // io.on("connect", (socket) => {
-  //   const engine = socket.io.engine;
-  //   console.log(engine.transport.name);
-  //   socket.emit("metadata",Spicetify.Queue.track.contextTrack.metadata)
-  // });
+  const socket = io("http://0.0.0.0:3000");
+  socket.on("connect", () => {
+    const engine = socket.io.engine;
+    console.log(engine.transport.name);
+    socket.emit("metadata",Spicetify.Queue.track.contextTrack.metadata)
+  });
   socket.on("command", async(data) => {
     // console.log(data)
     switch(data){
@@ -72,8 +64,6 @@ async function main() {
   //   console.log("HUH")
   //   socket.emit("command",Spicetify.Queue.track.contextTrack.metadata.title)
   // })
-  });
-  io.listen(3000);
 }
 
 export default main;
